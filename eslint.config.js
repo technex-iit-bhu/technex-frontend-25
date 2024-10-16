63d28cb101eff6825695d36e043fd800b8f5f59e
the-dg04 <devgup04@gmail.com>
2024-10-16 18:38:34 +0530

    ehh

--- eslint.config.js
+++ eslint.config.js
@@ -1,18 +1,20 @@
 export default[
     {
         "rules": {
-          "@typescript-eslint/no-unused-vars": [
-            "error",
-            {
-              "args": "all",
-              "argsIgnorePattern": "^_",
-              "caughtErrors": "all",
-              "caughtErrorsIgnorePattern": "^_",
-              "destructuredArrayIgnorePattern": "^_",
-              "varsIgnorePattern": "^_",
-              "ignoreRestSiblings": true
-            }
-          ]
+          "@typescript-eslint/no-unused-vars": "off",
+          "no-unused-vars": "off",
+        //   "@typescript-eslint/no-unused-vars": [
+        //     "error",
+        //     {
+        //       "args": "all",
+        //       "argsIgnorePattern": "^_",
+        //       "caughtErrors": "all",
+        //       "caughtErrorsIgnorePattern": "^_",
+        //       "destructuredArrayIgnorePattern": "^_",
+        //       "varsIgnorePattern": "^_",
+        //       "ignoreRestSiblings": true
+        //     }
+        //   ]
         }
       }
 ]
