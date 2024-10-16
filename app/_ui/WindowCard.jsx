16c10c5278c3e81d0587f708506662148f9a2bc0
the-dg04 <devgup04@gmail.com>
2024-10-16 16:06:59 +0530

    added WindowCard

---
+++ app/_ui/WindowCard.jsx
@@ -0,0 +1,17 @@
+export default function WindowCard(props){
+    return(<>
+        <div className="border border-black w-fit flex flex-col rounded-lg overflow-hidden relative m-10">
+            <div className="flex items-center pl-2 h-8 min-w-32 bg-gray-50">
+                <div className="w-4 h-4 rounded-full bg-red-500 mr-2"></div>
+                <div className="w-4 h-4 rounded-full bg-yellow-400 mr-2"></div>
+                <div className="w-4 h-4 rounded-full bg-green-600 mr-2"></div>
+                <div className="flex flex-1 justify-center items-center h-full">
+                    <div className="h-full flex justify-center items-center translate-x-[-40px] text-sm">{props.title}</div>
+                </div>
+            </div>
+            <div className="flex h-[200px] min-w-64 text-white bg-gray-800">
+                {props.children}
+            </div>
+        </div>
+    </>);
+}
