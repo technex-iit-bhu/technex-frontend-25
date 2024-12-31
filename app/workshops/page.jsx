21e15b309ef2e7fad12d951f514e52e527864c59
Abhinav Pandey <abhinav.pandey.met22@itbhu.ac.in>
2024-12-31 17:49:43 +0530

    Facing errors in subworkshops

--- app/workshops/page.jsx
+++ app/workshops/page.jsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import { useEffect, useState } from "react";
 import Background_E from "../_backgrounds/Background_E";
 import Navbar from "../_components/Navbar";
@@ -6,20 +6,23 @@ import WorkshopCard from "./workshopcard";
 import Footer from "../_components/Footer";
 
 const backendURL = process.env.NEXT_PUBLIC_BACKEND_URL;
-console.log("backend URL here : ", backendURL)
 
 export default function Workshops() {
   const [workshops, setWorkshops] = useState([]);
+  const formatDate = (dateString) => {
+    const options = { year: "numeric", month: "short", day: "numeric" };
+    const date = new Date(dateString);
+    return date.toLocaleDateString(undefined, options);
+  };
 
   useEffect(() => {
     fetch(`${backendURL}/api/workshops`)
       .then((response) => response.json())
       .then((data) => setWorkshops(data.workshops))
       .catch((error) => {
-        return console.error("Error fetching workshops:", error)
+        return console.error("Error fetching workshops:", error);
       });
   }, []);
-
   return (
     <>
       <Background_E />
@@ -33,9 +36,9 @@ export default function Workshops() {
               id={workshop.id}
               name={workshop.name}
               description={workshop.description}
-              sub_description={workshop.sub_description}
-              start_date={workshop.start_date}
-              end_date={workshop.end_date}
+              sub_description={workshop.subWorkshops?.[0]?.sub_desc}
+              start_date={workshop.subWorkshops?.[0]?.startDate}
+              end_date={workshop.subWorkshops?.[0]?.endDate}
             />
           ))}
         </div>
