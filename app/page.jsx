3bbc965fbefce16f32dcadb9d55cfbe1ecf14437
Abhinav Pandey <abhinav.pandey.met22@itbhu.ac.in>
2024-12-31 10:11:19 +0530

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -4,21 +4,33 @@ import Navbar from "./_components/Navbar";
 import HeroSection from "./_components/HeroSection";
 import FAQ from "./_components/FAQ";
 import Footer from "./_components/Footer";
-import {FireEffect} from "./_components/particleEffects"
+import { FireEffect } from "./_components/particleEffects";
 import { motion } from "framer-motion";
 
 export default function Home() {
+  const [windowDimensions, setWindowDimensions] = useState({
+    width: 0,
+    height: 0,
+  });
+
+  useEffect(() => {
+    setWindowDimensions({
+      width: window.innerWidth,
+      height: window.innerHeight,
+    });
+  }, []);
+
   return (
     <div className="absolute">
       <Background_A />
       <FireEffect />
       {[...Array(20)].map((_, i) => (
-      <motion.div
+        <motion.div
           key={i}
           className="absolute w-2 h-2 bg-yellow-500 opacity-50"
           initial={{
-            x: Math.random() * window.innerWidth,
-            y: Math.random() * window.innerHeight,
+            x: Math.random() * windowDimensions.width,
+            y: Math.random() * windowDimensions.height,
           }}
           animate={{
             y: [null, -20, 0],
