a046f099e99fc5b504f0b13d3904e723f06ea991
the-dg04 <devgup04@gmail.com>
2025-01-09 02:06:58 +0530

    added gallery images

--- app/gallery/page.jsx
+++ app/gallery/page.jsx
@@ -19,7 +19,7 @@ const GalleryModal = ({ onClick, children }) => {
   );
 };
 
-const GalleryCard = ({ imageSrc, expand }) => {
+const GalleryCard = ({ imageSrc, alt, expand, isExpanded, index }) => {
   return (
     <motion.div
       layoutId={imageSrc}
@@ -33,7 +33,7 @@ const GalleryCard = ({ imageSrc, expand }) => {
       whileInView="onscreen"
       variants={cardVariants}
       viewport={{ margin: "-80px 0px -10px 0px", amount: 0.1 }}
-      onClick={() => expand(imageSrc)}
+      onClick={() => expand(index)}
       className="w-[300px] lg:w-[533px] relative h-[160px] lg:h-[300px] flex flex-col justify-center items-center text-white"
     >
       <Image
@@ -47,22 +47,62 @@ const GalleryCard = ({ imageSrc, expand }) => {
           height: "auto",
         }}
       />
-      <div className="w-[265px] lg:w-[470px] mt-[15px] lg:mt-[10px] h-[160px] lg:h-[260px] bg-white rounded overflow-hidden"></div>
+      <img
+        src={imageSrc}
+        className="object-contain object-center w-[265px] lg:w-[470px] mt-[15px] lg:mt-[10px] h-[160px] lg:h-[260px] bg-white rounded overflow-hidden"
+      />
+      <div className="absolute bottom-[-10px] w-[300px] lg:w-[533px] flex justify-center">
+        <span className="text-black px-4 bg-white rounded-md bg-opacity-70">
+          {/* {isExpanded?"true":"false"} */}
+          {alt}
+        </span>
+      </div>
+      {/* <div
+        className={`absolute w-[100%] h-[100%] bg-gradient-to-r from-black/80 to-transparent left-0 ${
+          isExpanded ? "" : "hidden"
+        }`}
+      >
+        <div className="h-full w-[50%] overflow-y-auto no-scrollbar">
+          {image text}
+        </div>
+      </div> */}
     </motion.div>
   );
 };
 
 export default function Gallery() {
   let [expandedImage, setExpandedImage] = useState(null);
+
+  const images = [
+    { imageSrc: "/gallery/Sophie.png", alt: "Sophie" },
+    { imageSrc: "/gallery/AshneerGrover.png", alt: "Ashneer Grover" },
+    { imageSrc: "/gallery/GuruRandhawa.png", alt: "Guru Randhawa" },
+    { imageSrc: "/gallery/JubinNautiyal.png", alt: "Jubin Nautiyal" },
+    { imageSrc: "/gallery/KavithaSubramanian.jpg", alt: "Kavitha Subramanian" },
+    { imageSrc: "/gallery/LaxmiAgarwal.png", alt: "Laxmi Agarwal" },
+    { imageSrc: "/gallery/MikaSingh.png", alt: "Mika Singh" },
+    { imageSrc: "/gallery/NamitaThapar.png", alt: "Namita Thapar" },
+    { imageSrc: "/gallery/NitinVijay.png", alt: "Nitin Vijay" },
+    { imageSrc: "/gallery/RajivAnand.png", alt: "Rajiv Anand" },
+    { imageSrc: "/gallery/SandeepJain.png", alt: "Sandeep Jain" },
+    { imageSrc: "/gallery/SankarAnandPal.jpg", alt: "Sankar Anand Pal" },
+    { imageSrc: "/gallery/SatishDua.png", alt: "Satish Dua" },
+    { imageSrc: "/gallery/TechnicalGuruji.png", alt: "Technical Guruji" },
+  ];
+
   return (
     <div className="absolute">
-      {expandedImage && (
+      {expandedImage !== null && (
         <GalleryModal onClick={() => setExpandedImage(null)}>
           <motion.div
             className="scale-110 sm:scale-[2]"
             onClick={(e) => e.stopPropagation()}
           >
-            <GalleryCard imageSrc={expandedImage} expand={() => {}} />
+            <GalleryCard
+              {...images[expandedImage]}
+              isExpanded={true}
+              expand={() => {}}
+            />
           </motion.div>
         </GalleryModal>
       )}
@@ -73,12 +113,15 @@ export default function Gallery() {
         <div className="text-white px-20 py-20 text-5xl">Gallery</div>
         <div className="w-full flex items-center justify-center mb-24">
           <div className="max-w-[1200px] flex flex-wrap gap-20 justify-center items-center">
-            <GalleryCard imageSrc="dummy0.jpg" expand={setExpandedImage} />
-            <GalleryCard imageSrc="dummy1.jpg" expand={setExpandedImage} />
-            <GalleryCard imageSrc="dummy2.jpg" expand={setExpandedImage} />
-            <GalleryCard imageSrc="dummy3.jpg" expand={setExpandedImage} />
-            <GalleryCard imageSrc="dummy4.jpg" expand={setExpandedImage} />
-            <GalleryCard imageSrc="dummy5.jpg" expand={setExpandedImage} />
+            {images.map((image, idx) => (
+              <GalleryCard
+                {...image}
+                index={idx}
+                expand={setExpandedImage}
+                key={idx}
+                isExpanded={expandedImage === idx}
+              />
+            ))}
           </div>
         </div>
         <Footer />
